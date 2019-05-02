5e15846f89f12d6719ad4f701a7150ab7524e4e1
nevets615 <surj316@yahoo.com>
2019-05-02 16:26:18 -0400

    working my way through

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/dish.db3"
+    },
+
+    migrations: {
+      directory: ".data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    useNullAsDefault: true
+  }
+};
